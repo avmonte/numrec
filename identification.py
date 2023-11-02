f8c79a030eb7f51816ee4ffa4b6d69d26c53a0e2
avmonte <unstoppablehay@gmail.com>
2023-11-02 21:18:33 +0400

    code works poorly for now, needs improvement

---
+++ identification.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+from sys import argv
+
+path = argv[1]
+test = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+correlation = cv2.imread(f"matrices/{path.split('/')[-1].split('_')[1]}.png", cv2.IMREAD_GRAYSCALE)
+
+h, w = correlation.shape
+
+for i in range(h):
+	for j in range(w):
+		correlation[i, j] = (256 - abs(correlation[i, j] - test[i, j]))
+
+confidence = correlation.mean() / 256
+threshold = 0.5
+print(f"\nConfidence: {confidence}\nThreshold: {threshold}, thus ", end='')
+if confidence > threshold:
+	print(f"this is a {path.split('/')[-1].split('_')[1]}\n")
+else:
+	print(f"this is NOT a {path.split('/')[-1].split('_')[1]}\n")
